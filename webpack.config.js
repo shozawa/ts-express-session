bbfde7a20232bccdd997dd3e61f9c85763d2e70e
shozawa <tomohiro.shozawa@gmail.com>
2019-09-21 16:33:26 +0900

    webpack-dev-server でファイルをホストしてみる

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 // const path = require("path");
 // const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/client/index.ts",
   devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
   output: {
     filename: "index.js",
     publicPath: "/"
   },
+  plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
       {
