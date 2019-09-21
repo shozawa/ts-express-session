921b8576e806b9c6e527eaead20a2fb5978e04c2
shozawa <tomohiro.shozawa@gmail.com>
2019-09-22 08:12:51 +0900

    フロント側で ping を実装

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,2 +1,8 @@
-// eslint-disable-next-line no-console
-console.log("Hello, TypeScript.");
+import { axiosInstance } from "./api";
+import { Health } from "../types/api";
+
+document.getElementById("ping")!.addEventListener("click", async () => {
+  const { data } = await axiosInstance.get<Health>("/ping");
+  const counter = document.getElementById("count")!;
+  counter.innerHTML = `${data.count}`;
+});
