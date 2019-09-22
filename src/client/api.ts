db0b0d4fb85d61d1040fd8c1ebcefd6cdd8e68b1
shozawa <tomohiro.shozawa@gmail.com>
2019-09-22 13:59:35 +0900

    クライアント側にも Lookup Types を利用して型を付ける

--- src/client/api.ts
+++ src/client/api.ts
@@ -1,8 +1,16 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { APP_HOST, APP_PORT } from "../constants";
+import { GET } from "../types/get";
 
 export const axiosInstance = axios.create({
   baseURL: `http://${APP_HOST}:${APP_PORT}`,
   headers: { "Content-Type": "application/json" },
   responseType: "json"
 });
+
+export function apiGet<T extends keyof GET>(
+  path: T,
+  config?: AxiosRequestConfig
+) {
+  return axiosInstance.get<GET[T]["res"]>(path, config);
+}
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,8 +1,7 @@
-import { axiosInstance } from "./api";
-import { Health } from "../types/api";
+import { apiGet } from "./api";
 
 document.getElementById("ping")!.addEventListener("click", async () => {
-  const { data } = await axiosInstance.get<Health>("/ping");
+  const { data } = await apiGet("/ping");
   const counter = document.getElementById("count")!;
   counter.innerHTML = `${data.count}`;
 });
